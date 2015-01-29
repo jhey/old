2806804b179ff42f92684f6d792841c9a9a2a3ac
julio <julior@gmail.com>
2015-01-29 09:27:26 -0600

    FULL CRUD
    todo app

--- js_jquery/js_jquery/js/scripts.js
+++ js_jquery/js_jquery/js/scripts.js
@@ -1,4 +1,4 @@
-// jQuery TODO APP w/Firebase v1.02
+// jQuery TODO APP w/Firebase v1.04
 // Make sure DOM & jQuery are loaded...
 $(function () {
 
@@ -9,99 +9,54 @@ $(function () {
 // Hold all of the TODO objects
 var todoHolder = [];
 
-// Todo currently being edited - Object
-// @ null = not editing, else is editing
+// temp todo holder
 var currTodo = null;
 
 // Todo currently being edited - Index
 // @ -1 = not editing, else is editing
 var currTodoIndex = -1;
 
-    // TODO construcutor
-    function Todo(task, completed) {
+    // TODO constructor
+    function Todo(task, completed, date) {
         this.task = task;
-        this.competed = completed;
-        this.timeAdded = new Date();
+        this.completed = completed;
+        this.timeAdded = date;
     }
 
-    // handle form events...
+    // handle events from the INPUT FORM...
     $("#todoForm").submit(function (event) {
         event.preventDefault(); // Prevent defualt browser action
         var todoTask = $("#fTodo").val();
+        if (todoTask === "") { return }  // if field is not empty
         $("#fBtn").attr("value", "Add");
-
         if (currTodoIndex !== -1) { // perform an UPDATE
-            todoHolder[currTodoIndex].task = todoTask;
+            updateTodoCall(); // update via ajax...
             currTodoIndex = -1;
             currTodo = null;
         } else { // perform an ADD
-
-            if (todoTask !== "") { // if field is not empty
-                //var tempTodo = new Todo(todoTask, false);
-                //todoHolder.push(tempTodo);
-                //ajax...
-                addTodoCall();
-            }
-            
+            addTodoCall();    
         }
-        showTodos();
+        $("#fTodo").val("");
     });
 
-    // READ the contents of Array
-    function showTodos() {
-        $("#myList").empty();
-        var len = todoHolder.length;
-        var str;       // String for the Delete
-        var idStr;     // String for the IDhbjb
-        for (var i = 0; i < len; i++) { 
-            idStr = i;
-            str = "<a href='#' onclick='deleter("+idStr+")'>X</a>"
-            $("#myList").append("<li id=a'" + idStr + "'><a onclick='editTodo(" + idStr + ")' >" + todoHolder[i].task + "</a> - " + str + "</li>")
-        }
-    }
 
     // DELETE Method for the todo
     function deleter(idClicked) {
-        todoHolder.splice(idClicked, 1)
-        showTodos();
+        currTodoIndex = idClicked;
+        deleteTodoCall();
     }
 
     // UPDATE Method for the todo
     function editTodo(idClicked) {
         currTodoIndex = idClicked;
-        var editTodo = todoHolder[currTodoIndex];
-        var task = editTodo.task;
-        $("#fTodo").val(task);
+        var tTask = todoHolder[currTodoIndex].task;
+        $("#fTodo").val(tTask);
         $("#fBtn").attr("value", "Update");
-        console.log("clicked edit", task); 
+        //
+        console.log("clicked edit!!!", idClicked, todoHolder[currTodoIndex].id); 
     }
 
-
-    // handle click event on link #fTrigger
-    $("#fTrigger").click(function () {
-        $("#todoForm").submit(); // submit form
-    });
-    // handle click event for link #hyper
-    $("#hyper").click(function () {
-        alert("this is hyper!");// alert
-        $("#todoForm").submit(); // call the submit on #todoForm
-    });
-
-
-    // handle click event for link #ajaxCaller
-    $("#ajaxCaller").click(function (event) {
-        event.preventDefault(); // Prevent defualt browser action
-        readTodoCall();
-    });
-
-    // handle click event for Add to firebase
-    $("#ajaxAddCaller").click(function (event) {
-        event.preventDefault(); // Prevent defualt browser action
-        addTodoCall();
-    });
-
-
-    // Make AJAX call - to READ all data
+///////////////////////////// READ -  Make AJAX call - to READ all data
     function readTodoCall() {
         var myrequest = new XMLHttpRequest();
         myrequest.open("GET", "https://cccontact.firebaseio.com/.json", true);
@@ -120,40 +75,40 @@ var currTodoIndex = -1;
     // render the firebase content to the screen
     function renderContent(tempData) {
         var firebaseData = JSON.parse(tempData);
-        console.log(firebaseData);
+        todoHolder = [];
         var counter = 0;
         $("#myList").empty();
-        for (var i in firebaseData) {
+        for (var i in firebaseData) { // parse the FBData into an Array
             console.log(i, firebaseData[i].task);
             //
             var len = todoHolder.length;
-            var str;       // String for the Delete
-            var idStr;     // String for the IDhbjb
-            
+            var str;       // String for the Delete - STRING/HTML
+            var idStr;     // String for the ID - NUMBER
             idStr = counter;
+            // build object..
+            var tdos = new Todo(firebaseData[i].task, firebaseData[i].completed, firebaseData[i].timeAdded);
+            tdos.__proto__ = { id: i };
+            todoHolder.push(tdos);
+            // RENDER to view...
             str = "<a href='#' onclick='deleter(" + idStr + ")'>X</a>"
-            $("#myList").append("<li id=a'" + idStr + "'><a onclick='editTodo(" + idStr + ")' >" + firebaseData[i].task + "</a> - " + str + "</li>")
-            //
+            $("#myList").append("<li id='a" + idStr + "'><a onclick='editTodo(" + idStr + ")' >" + firebaseData[i].task + "</a> - " + str + "</li>")
             counter++
         }
+        console.log(todoHolder);
     }
 
-// make AJAX to add data
-
+///////////////////////////// CREATE - make AJAX to add data
     function addTodoCall() {
-        var objToAdd = {};
-        objToAdd.task = $("#fTodo").val(); // grab text from field
-        objToAdd.timeAdded = new Date();
-        objToAdd.isCompleted = false;
-
+        var t_task = $("#fTodo").val();
+        var t_complete = false;
+        var t_date = new Date();
+        var objToAdd = new Todo(t_task, t_complete, t_date);
         var myrequest = new XMLHttpRequest();
         myrequest.open("POST", "https://cccontact.firebaseio.com/.json", true);
         myrequest.onload = function () {
             if (this.status >= 200 && this.status < 400) { // success
-                console.log("it was a success");
-                console.log("DATA", this.response);
-                // Reload data after success
-                readTodoCall();
+                console.log("POST was a success", this.response);
+                readTodoCall(); // Reload data after success
             } else { // problem
                 console.log("there was a problem");
             }
@@ -162,3 +117,39 @@ var currTodoIndex = -1;
         myrequest.send(jsonToSend); // send to firebase
     }
 
+///////////////////////////// UPDATE - update the current item
+    function updateTodoCall() {
+        var objToAdd = new Todo($("#fTodo").val(), todoHolder[currTodoIndex].completed, todoHolder[currTodoIndex].timeAdded);
+        console.log(objToAdd);
+        var itemToUpdate = todoHolder[currTodoIndex].id;
+        var putString = "https://cccontact.firebaseio.com/"+ itemToUpdate +"/.json"
+        var myrequest = new XMLHttpRequest();
+        myrequest.open("PUT", putString, false);
+        myrequest.onload = function () {
+            if (this.status >= 200 && this.status < 400) { // success
+                console.log("PUT was a success", this.response);
+                readTodoCall(); // Reload data after success
+            } else { // problem
+                console.log("there was a problem");
+            }
+        };
+        var jsonToSend = JSON.stringify(objToAdd); // covert to string
+        myrequest.send(jsonToSend); // send to firebase
+    }
+
+    ///////////////////////////// DELETE - delete the current item
+    function deleteTodoCall() {
+        var itemToUpdate = todoHolder[currTodoIndex].id;
+        var putString = "https://cccontact.firebaseio.com/" + itemToUpdate + "/.json";
+        var myrequest = new XMLHttpRequest();
+        myrequest.open("DELETE", putString, true);
+        myrequest.onload = function () {
+            if (this.status >= 200 && this.status < 400) { // success
+                console.log("DELETE was a success", this.response);
+                readTodoCall(); // Reload data after success
+            } else { // problem
+                console.log("there was a problem");
+            }
+        };
+        myrequest.send(); // send to firebase
+    }
