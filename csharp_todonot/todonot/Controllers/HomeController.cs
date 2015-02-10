a5c2f5a5865a454fdd25342049613e2673940d36
julior@gmail.com <julior@gmail.com>
2015-02-10 15:59:22 -0600

    NotTodo app
    - csharp NotTodo app
    - Razor intro

---
+++ csharp_todonot/todonot/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using todonot.Models;
+
+namespace todonot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Welcome the the About page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Call me @ 555-55-5555";
+            return View();
+        }
+
+        public ActionResult Create() {
+            CreateTodoViewModel model = new CreateTodoViewModel();
+            model.Todos = PopulateTodo();
+            ViewBag.Message = "Create someting to Not do.";
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Create(CreateTodoViewModel model) {
+            model.Todos = PopulateTodo();
+            model.Todos.Add(new TodoViewModel {
+                Text = model.Text,
+                dateCompleted = DateTime.Now,
+                isCompleted = false
+            });
+            return View(model);
+        }
+
+
+        public ActionResult Nottodo() {
+            ViewBag.Message = "Do not do this...";
+            HomeViewModel model = new HomeViewModel();
+            model.Name = "NOT!";
+            model.Todos = PopulateTodo();
+            return View(model);
+        }
+
+        // Method to pre populate a list of todos
+        private static List<TodoViewModel> PopulateTodo() {
+            List<TodoViewModel> model = new List<TodoViewModel>();  // List of todos
+            model.Add(new TodoViewModel {  // add one todo
+                Text = "trip and fall", 
+                isCompleted = true, 
+                dateCompleted = new DateTime()
+            });
+            model.Add(new TodoViewModel {  // add one todo
+                Text = "fail phase 2",
+                isCompleted = false,
+                dateCompleted = new DateTime()
+            });
+            return model;
+        }
+
+
+    }
+}
---
+++ csharp_todonot/todonot/Models/HomeViewModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace todonot.Models
+{
+    public class HomeViewModels
+    {
+    }
+
+    public class TodoViewModel {
+        public string Text { get; set; }
+        public bool isCompleted { get; set; }
+        public DateTime dateCompleted { get; set; }
+        
+    }
+
+    public class HomeViewModel {
+        public string Name { get; set; }
+        public List<TodoViewModel> Todos { get; set; }
+    }
+
+    public class CreateTodoViewModel    {
+        [DisplayName("Do not do the following:")]
+        public string Text { get; set; }
+        public List<TodoViewModel> Todos { get; set; }
+    }
+
+
+}
