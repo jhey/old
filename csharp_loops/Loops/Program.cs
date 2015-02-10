9da2fcda942e6925efc87c169b87e14736b0dbba
julior@gmail.com <julior@gmail.com>
2015-02-10 10:40:11 -0600

    even loops
    loop types to output even numbers

---
+++ csharp_loops/Loops/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 20) 
+            {
+                if (i % 2 == 0) 
+                {
+                   // Console.WriteLine(i);
+                }
+                i++;
+            }
+           // Console.ReadLine();
+            for (int j = 0; j < 21; j++)
+
+           {
+              if(j % 2 == 0){
+                  Console.WriteLine(j);
+              }  
+            };
+            //Console.ReadLine() 
+
+            //int[]
+            List<int> numbers = new List<int>();
+            for (int z = 0; z < 21; z++)
+            {
+                numbers.Add(z);
+            }
+            foreach (var item in numbers)
+            {
+                
+                if(item %2==0){
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
